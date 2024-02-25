c927f58d47c0b58505474e77c8609cfb8a65df4e
Sergio Castelani <sergio.castelani@gmail.com>
2024-02-25 15:56:17 -0300

    feat: initial pokedex page

---
+++ src/pages/PokedexPage.tsx
@@ -0,0 +1,19 @@
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { PokeCardList } from "../components/PokeCardList";
+import { pokedexActions } from "../redux/slices/pokedex.slice";
+
+export const PokedexPage = () => 
+{
+    const pokedex = useAppSelector(store => store.pokedex);
+    const dispatch = useAppDispatch();
+
+    return (
+        <>
+            <PokeCardList 
+                pagination={pokedex}
+                onPageChange={page => dispatch(pokedexActions.changePage({page, pageSize: pokedex.pageSize}))}
+                onPageSizeChange={pageSize => dispatch(pokedexActions.changePage({page: 1, pageSize}))}
+            />
+        </>
+    );
+};
--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import { HomePage } from "./HomePage";
+import { PokedexPage } from "./PokedexPage";
 
 export const appRouter = createBrowserRouter([
     {
         path: "/",
         element: <HomePage/>,
+    },
+    {
+        path: "/pokedex",
+        element: <PokedexPage/>,
     },
 ]);
 
